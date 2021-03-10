9cbadaa48eb09204923d6fc746839cf3cca5fe15
EnriquePSouza <enriquepdsz@gmail.com>
2021-03-10 11:15:29 -0300

    Organized layers and finalized bank structure

--- ProAgil.API/Program.cs
+++ ProAgil.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ProAgil.API
+namespace ProAgil.Api
 {
     public class Program
     {
